15db09c62ac49db5120018f1b8237afd7b0e6554
Mauricio Negro <90003489+mauriinegro@users.noreply.github.com>
2026-03-18 16:44:10 -0300

    Update Refreshable.java

--- src/main/java/com/dimenna/truck/ui/Refreshable.java
+++ src/main/java/com/dimenna/truck/ui/Refreshable.java
@@ -1,9 +1,9 @@
 package com.dimenna.truck.ui;
 
 public interface Refreshable {
-    /** Se llama cada vez que la vista vuelve a mostrarse. */
+    
     default void onShow() {}
 
-    /** Se llama cuando cambió el catálogo (ABM o reorden). */
+    
     default void onCatalogChanged() {}
 }
